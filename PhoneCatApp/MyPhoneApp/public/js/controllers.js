29e223ca1719636a79715150e03bf938b718b7a8
张明凯 <zhangmingkai19892013@gmail.com>
2015-04-18 12:09:18 +0800

    Add More filters ? sort things

--- PhoneCatApp/MyPhoneApp/public/js/controllers.js
+++ PhoneCatApp/MyPhoneApp/public/js/controllers.js
@@ -2,13 +2,16 @@ var MyPhoneApp=angular.module('MyPhoneApp', []);
 MyPhoneApp.controller('PhoneListCtrl', function($scope){
 	$scope.phones=[{
 		'name':'Nexus S',
-		'snippet':'Fast Just Got Faster With Nexus S'
+		'snippet':'Fast Just Got Faster With Nexus S',
+		'age':2011
 	},{
 		'name':'Motorola Xoom',
-		'snippet':'The next,next generation tablet'
+		'snippet':'The next,next generation tablet',
+		'age':2014
 	},{
 		'name':'Moto Phone',
-		'snippet':'The best phone forever'
+		'snippet':'The best phone forever',
+		'age':2012
 	}
 	];
 });

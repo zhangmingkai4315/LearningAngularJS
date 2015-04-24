f581008914f6c53670ffc8cda4d245481f993550
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-04-24 19:22:25 +0800

    增加主页
    增加主页及基本的元素

---
+++ WeeklyReport/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+app.use('/static', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile('./public/index.html');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
